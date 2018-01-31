5afc696a87d10150c209bb82654e068c25dff2f4
Suraj Shah <ssshah22@asu.edu>
2018-01-31 13:36:21 -0700

    Everything except graphview done

--- group5/app/src/main/java/com/group5project/mobilecomputing/group5/HomeActivity.java
+++ group5/app/src/main/java/com/group5project/mobilecomputing/group5/HomeActivity.java
@@ -1,13 +1,37 @@
 package com.group5project.mobilecomputing.group5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
+    int backButtonCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    /**
+     * Back button listener.
+     * https://stackoverflow.com/questions/2354336/android-pressing-back-button-should-exit-the-app
+     */
+
+    @Override
+    public void onBackPressed()
+    {
+        if(backButtonCount >= 1)
+        {
+            finish();
+            super.onBackPressed();
+        }
+        else
+        {
+            Toast.makeText(this, "Please press the back button again to exit the application.", Toast.LENGTH_SHORT).show();
+            backButtonCount++;
+        }
+    }
+
 }
