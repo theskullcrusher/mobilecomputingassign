632f0c21f3662b1489d1f102edb7fe60d0776ca3
Suraj Shah <ssshah22@asu.edu>
2018-01-31 23:54:55 -0700

    Minimal working sine graph

--- group5/app/src/main/java/com/group5project/mobilecomputing/group5/HomeActivity.java
+++ group5/app/src/main/java/com/group5project/mobilecomputing/group5/HomeActivity.java
@@ -5,19 +5,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.concurrent.TimeUnit;
+
 public class HomeActivity extends AppCompatActivity {
 
+    //more than one press exits the app and finishes the activity
     int backButtonCount = 0;
+
+    private LineGraphSeries<DataPoint> series1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        double x,y;
+        x = 0;
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series1 = new LineGraphSeries<DataPoint>();
+
+        int numPoints = 500;
+
+        for(int i=0;i<numPoints;i++){
+            x = x + 0.1;
+            y = Math.sin(x);
+            series1.appendData(new DataPoint(x,y), true, 100);
+        }
+        graph.addSeries(series1);
     }
 
     /**
      * Back button listener.
      * https://stackoverflow.com/questions/2354336/android-pressing-back-button-should-exit-the-app
      */
+
+
+    public void nullifyBackPress() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5);
+        backButtonCount--;
+    }
 
     @Override
     public void onBackPressed()
@@ -31,6 +62,11 @@ public class HomeActivity extends AppCompatActivity {
         {
             Toast.makeText(this, "Please press the back button again to exit the application.", Toast.LENGTH_SHORT).show();
             backButtonCount++;
+            try {
+                nullifyBackPress();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
