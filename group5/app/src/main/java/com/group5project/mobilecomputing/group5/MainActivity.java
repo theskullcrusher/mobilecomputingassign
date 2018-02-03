c757cdd8dd9cc0f98eda44a25b2e88a991aca1e1
srinijakanteti <35789934+srinijakanteti@users.noreply.github.com>
2018-02-02 19:45:31 -0700

    Revert "Update MainActivity.java"

--- group5/app/src/main/java/com/group5project/mobilecomputing/group5/MainActivity.java
+++ group5/app/src/main/java/com/group5project/mobilecomputing/group5/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(homeIntent);
-                //displays the splash screen for 4000ms before switching to HomeActivity
                 finish();
             }
         },SPLASHSCREENTIMEOUT);
