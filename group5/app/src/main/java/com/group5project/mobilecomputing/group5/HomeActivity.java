d791ca9fc91fdd2a724e8671e25b4560d01592f5
srinijakanteti <35789934+srinijakanteti@users.noreply.github.com>
2018-02-06 14:26:30 -0700

    Update HomeActivity.java
    Working graph with clear on STOP and updated graph on RUN.
    Added comments

--- group5/app/src/main/java/com/group5project/mobilecomputing/group5/HomeActivity.java
+++ group5/app/src/main/java/com/group5project/mobilecomputing/group5/HomeActivity.java
@@ -1,28 +1,31 @@
 package com.group5project.mobilecomputing.group5;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class HomeActivity extends AppCompatActivity {
 
-    //more than one press exits the app and finishes the activity
+    //more than one press of back button exits the app and finishes the activity
     int backButtonCount = 0;
 
     private LineGraphSeries<DataPoint> series1;
+    GraphView graph;
+    private DataPoint[] data;
     private boolean run_flag = false;
+    private boolean clear_data = false;
     private static final Random RANDOM = new Random();
+    private int SPIKE_PERIOD = 20;
+    private double SPIKE_AMPLITUDE = 40.0;
     private double lastX = 0.0;
     private Button run;
     private Button stop;
@@ -37,30 +40,25 @@ public class HomeActivity extends AppCompatActivity {
 
         double x,y;
         x = 0.0;
-        GraphView graph = (GraphView) findViewById(R.id.graph);
+        graph = (GraphView) findViewById(R.id.graph);
         series1 = new LineGraphSeries<DataPoint>();
         graph.addSeries(series1);
         Viewport viewport = graph.getViewport();
         viewport.setYAxisBoundsManual(true);
         viewport.setMinY(0.0);
-        viewport.setMaxY(1.0);
+        viewport.setMaxY(50.0);
         viewport.setMinX(0.0);
         viewport.setMaxX(100.0);
         viewport.setScalable(true);
         viewport.setScrollable(true);
 
-//        int numPoints = 15000;
-//
-//        for(int i=0;i<numPoints;i++){
-//            x = x + 0.1;
-//            y = Math.sin(x);
-//            series1.appendData(new DataPoint(x,y), true, 1000);
-//        }
-
         run.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 run_flag = true;
+                clear_data = false;
+                //adds the series to the graph whenever run is pressed; shows the updated graph on clicking run after clear
+                graph.addSeries(series1);
             }
         });
 
@@ -68,12 +66,10 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 run_flag = false;
+                clear_data = true;
             }
         });
-
-
     }
-
 
     @Override
     protected void onResume() {
@@ -83,6 +79,7 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void run() {
                 while (true) {
+                    //keeps updating and drawing the graph when run button is pressed
                     while (run_flag) {
                         runOnUiThread(new Runnable() {
                             @Override
@@ -97,10 +94,27 @@ public class HomeActivity extends AppCompatActivity {
                             e.printStackTrace();
                         }
                     }
+                    /*
+                     *clears the graph when stop button is pressed
+                     * https://stackoverflow.com/questions/15913017/graphview-and-resetdata
+                     */
+                    if(clear_data) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                graph.removeAllSeries();
+                            }
+                        });
+                        //Sleep for short time to show updates on screen
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
         }).start();
-
     }
 
     /**
@@ -108,19 +122,17 @@ public class HomeActivity extends AppCompatActivity {
      * Turn flag to false to stop adding any data
      */
     public void updateGraph(){
-        double lastY = Math.sin(lastX);
+        /**
+         * To simulate ECG type of graph
+         * https://stackoverflow.com/questions/23167879/java-how-to-simulate-an-ecg-electro-cardiogram
+         */
+        double lastY = RANDOM.nextDouble() * 5d;
+        if (lastX % SPIKE_PERIOD == 0) {
+            lastY = SPIKE_AMPLITUDE;
+        }
         series1.appendData(new DataPoint(lastX, lastY), true, 1000);
-        lastX+=0.1;
+        lastX += 2;
     }
-
-
-
-//    public void nullifyBackPress() throws InterruptedException {
-//        TimeUnit.SECONDS.sleep(5);
-//        backButtonCount--;
-//    }
-
-
     /**
      * Back button listener.
      * https://stackoverflow.com/questions/2354336/android-pressing-back-button-should-exit-the-app
@@ -140,5 +152,4 @@ public class HomeActivity extends AppCompatActivity {
             backButtonCount++;
         }
     }
-
 }
