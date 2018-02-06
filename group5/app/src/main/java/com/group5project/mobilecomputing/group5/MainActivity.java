1035853025120e85aff8557043e5259ad97b46ce
srinijakanteti <35789934+srinijakanteti@users.noreply.github.com>
2018-02-06 14:14:08 -0700

    Update MainActivity.java
    Added comments

--- group5/app/src/main/java/com/group5project/mobilecomputing/group5/MainActivity.java
+++ group5/app/src/main/java/com/group5project/mobilecomputing/group5/MainActivity.java
@@ -4,12 +4,10 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.srinija.group5.R;
 
 public class MainActivity extends AppCompatActivity {
-
     private static int SPLASHSCREENTIMEOUT = 4000;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
